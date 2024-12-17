c1cf39e4aeef8573fe0f762b5c80257c750b513d
henry <1151071+HenrYxZ@users.noreply.github.com>
2024-12-17 18:57:40 -0300

    feat: face colors

--- main.c
+++ main.c
@@ -31,7 +31,8 @@ void setup(void) {
 		window_width,
 		window_height
 	);
-	load_obj_file("./assets/cube.obj");
+	//load_obj_file("./assets/cube.obj");
+	load_cube_mesh_data();
 }
 
 void handle_input(void) {
@@ -147,19 +148,25 @@ void update(void) {
 			}
 		}
 
-		triangle_t projected_triangle;
-
 		// Loop all three vertices to perform projection
+		vec2_t projected_points[3];
 		for (int j = 0; j < 3; j++) {
 			// Project the current vertex
-			vec2_t projected_point = project(transformed_vertices[j]);
+			projected_points[j] = project(transformed_vertices[j]);
 
 			// Scale and translate the projected points to the middle of the screen
-			projected_point.x += (window_width / 2);
-			projected_point.y += (window_height / 2);
-
-			projected_triangle.points[j] = projected_point;
+			projected_points[j].x += (window_width / 2);
+			projected_points[j].y += (window_height / 2);
 		}
+
+		triangle_t projected_triangle = {
+			.points = {
+				{ projected_points[0].x, projected_points[0].y },
+				{ projected_points[1].x, projected_points[1].y },
+				{ projected_points[2].x, projected_points[2].y }
+			},
+			.color = mesh_face.color
+		};
 
 		// Save the projected triangle in the array of triangles to render
 		array_push(triangles_to_render, projected_triangle);
@@ -184,7 +191,7 @@ void render(void) {
 				triangle.points[1].y,
 				triangle.points[2].x,
 				triangle.points[2].y,
-				0xFF555555
+				triangle.color
 			);
 		}
 		
