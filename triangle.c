88404998998538928087193cc1c6ae7d81432a37
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-03-29 17:48:43 -0300

    feat: perspective correct textures

--- triangle.c
+++ triangle.c
@@ -31,23 +31,38 @@ void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint
 	}
 }
 
-void draw_texel(int x, int y, uint32_t* texture, vec2_t a, vec2_t b, vec2_t c, float u0, float v0, float u1, float v1, float u2, float v2)
-{
+void draw_texel(
+	int x, int y, uint32_t* texture,
+	vec4_t a, vec4_t b, vec4_t c,
+	tex2_t a_uv, tex2_t b_uv, tex2_t c_uv
+) {
 	vec2_t p = { x, y };
-	vec3_t weights = barycentric_weights(a, b, c, p);
+	vec2_t proj_a = vec2_from_vec4(a);
+	vec2_t proj_b = vec2_from_vec4(b);
+	vec2_t proj_c = vec2_from_vec4(c);
+	vec3_t weights = barycentric_weights(proj_a, proj_b, proj_c, p);
 	float alpha = weights.x;
 	float beta = weights.y;
 	float gamma = weights.z;
 
-	float interpolated_u = alpha * u0 + beta * u1 + gamma * u2;
-	float interpolated_v = alpha * v0 + beta * v1 + gamma * v2;
+	float interpolated_u;
+	float interpolated_v;
+	float interpolated_reciprocal_w;
+	
+	// To have perspective correct uv interpolation we use 1 / w
+	interpolated_u = alpha * (a_uv.u / a.w) + beta * (b_uv.u / b.w) + gamma * (c_uv.u / c.w);
+	interpolated_v = alpha * (a_uv.v / a.w) + beta * (b_uv.v / b.w) + gamma * (c_uv.v / c.w);
+	interpolated_reciprocal_w = alpha / a.w + beta / b.w + gamma / c.w;
+
+	interpolated_u /= interpolated_reciprocal_w;
+	interpolated_v /= interpolated_reciprocal_w;
 
 	int tex_x = abs((int)(interpolated_u * texture_width));
 	int tex_y = abs((int)(interpolated_v * texture_height));
 
 	int index = tex_x + (tex_y * texture_width);
 	//if (index < 0 || index >= texture_height * texture_width) {
-	//	sprintf_s("ERROR: index %d out of texture bounds", index);
+	//	sprintf("ERROR: index %d out of texture bounds", index);
 	//}
 	draw_pixel(x, y, texture[index]);
 }
@@ -84,9 +99,9 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
 
 
 void draw_textured_triangle(
-	int x0, int y0, float u0, float v0,
-	int x1, int y1, float u1, float v1,
-	int x2, int y2, float u2, float v2,
+	int x0, int y0, float z0, float w0, float u0, float v0,
+	int x1, int y1, float z1, float w1, float u1, float v1,
+	int x2, int y2, float z2, float w2, float u2, float v2,
 	uint32_t* texture
 )
 {
@@ -94,26 +109,35 @@ void draw_textured_triangle(
 	if (y0 > y1) {
 		int_swap(&y0, &y1);
 		int_swap(&x0, &x1);
+		float_swap(&z0, &z1);
+		float_swap(&w0, &w1);
 		float_swap(&u0, &u1);
 		float_swap(&v0, &v1);
 	}
 	if (y1 > y2) {
 		int_swap(&y1, &y2);
 		int_swap(&x1, &x2);
+		float_swap(&z1, &z2);
+		float_swap(&w1, &w2);
 		float_swap(&u1, &u2);
 		float_swap(&v1, &v2);
 	}
 	if (y0 > y1) {
 		int_swap(&y0, &y1);
 		int_swap(&x0, &x1);
+		float_swap(&z0, &z1);
+		float_swap(&w0, &w1);
 		float_swap(&u0, &u1);
 		float_swap(&v0, &v1);
 	}
 
-	// Create vector points
-	vec2_t a = { x0, y0 };
-	vec2_t b = { x1, y1 };
-	vec2_t c = { x2, y2 };
+	// Create vector points and texture coords
+	vec4_t a = { x0, y0, z0, w0 };
+	vec4_t b = { x1, y1, z1, w1 };
+	vec4_t c = { x2, y2, z2, w2 };
+	tex2_t a_uv = { u0, v0 };
+	tex2_t b_uv = { u1, v1 };
+	tex2_t c_uv = { u2, v2 };
 
 	// Render the upper part of the triangle (flat-bottom)
 	float inv_slope_1 = 0;
@@ -132,7 +156,7 @@ void draw_textured_triangle(
 			}
 
 			for (int x = x_start; x <= x_end; x++) {
-				draw_texel(x, y, texture, a, b, c, u0, v0, u1, v1, u2, v2);
+				draw_texel(x, y, texture, a, b, c, a_uv, b_uv, c_uv);
 			}
 		}
 	}
@@ -154,7 +178,7 @@ void draw_textured_triangle(
 			}
 
 			for (int x = x_start; x <= x_end; x++) {
-				draw_texel(x, y, texture, a, b, c, u0, v0, u1, v1, u2, v2);
+				draw_texel(x, y, texture, a, b, c, a_uv, b_uv, c_uv);
 			}
 		}
 	}
