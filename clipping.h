99434148cb92c55094e132df3e4a8d3617613934
henry <1151071+HenrYxZ@users.noreply.github.com>
2025-07-04 18:03:23 -0400

    wip: clipping planes definition

---
+++ clipping.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "vector.h"
+
+enum {
+	LEFT_FRUSTRUM_PLANE,
+	RIGHT_FRUSTRUM_PLANE,
+	TOP_FRUSTRUM_PLANE,
+	BOTTOM_FRUSTRUM_PLANE,
+	NEAR_FRUSTRUM_PLANE,
+	FAR_FRUSTRUM_PLANE
+};
+
+typedef struct {
+	vec3_t point;
+	vec3_t normal;
+} plane_t;
